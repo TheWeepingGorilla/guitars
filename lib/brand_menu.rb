c6a9d2cc4be6501d11bfc03dd0ccdb81c6f263df
Sean Morrison <student@epicodus.com>
2014-08-09 15:39:47 -0700

    Add delete and list manufacturers.  Manufacturer menu placed in seperate file.

---
+++ lib/brand_menu.rb
@@ -0,0 +1,46 @@
+def brand_menu
+  linespace
+  puts "A > Add a manufacturer"
+  puts "L > List manufacturers"
+  puts "D > Delete a manufacturer"
+  puts "R > Return to main menu."
+  linespace
+  choice = gets.chomp.upcase
+  case(choice)
+    when 'A'
+      linespace
+      puts "Manufacturer name:"
+      brand = gets.chomp
+      new_brand = Brand.new({:brand => brand})
+      new_brand.save
+      puts "#{new_brand.brand} has been added!"
+      brand_menu
+    when 'L'
+      linespace
+      results = Brand.all
+      puts "All Manufacturers:"
+      results.each do |result|
+        puts "Brand: #{result.brand}"
+        linespace
+      end
+      brand_menu
+    when 'D'
+      linespace
+      puts "Manufacturer to remove:"
+      brand_to_remove = gets.chomp
+      results = Brand.all
+      results.each do |result|
+        if result.brand == brand_to_remove
+          puts "Brand #{result.brand} has been removed."
+          result.delete
+        else
+          puts "Result not found.  Did you type the name correctly?"
+        end
+      end
+      brand_menu
+    when 'R'
+      main_menu
+    else
+      puts "Invalid! Try again"
+  end
+end
