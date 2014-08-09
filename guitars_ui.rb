cc3f735123e4a728d230b2fe65c6c08120f71c6d
Sean Morrison <student@epicodus.com>
2014-08-09 15:33:52 -0700

    Add manufacturer menu.

--- guitars_ui.rb
+++ guitars_ui.rb
@@ -34,13 +34,13 @@ end
 def main_menu
   loop do
     linespace
-    puts "M > Maunfacturer Menu"
+    puts "M > Manufacturer Menu"
     puts "B > Body Type Menu"
     puts "G > Guitar Menu"
     puts "X > Exit."
     linespace
-    main_choice = gets.chomp.upcase
-    case(main_choice)
+    choice = gets.chomp.upcase
+    case(choice)
       when 'M'
         brand_menu
       when 'B'
@@ -56,6 +56,52 @@ def main_menu
   end
 end
 
+def brand_menu
+  linespace
+  puts "A > Add a manufacturer"
+  puts "L > List manufacturers"
+  puts "D > Delete a manufacturer"
+  puts "R > Return to main menu."
+  linespace
+  choice = gets.chomp.upcase
+  case(choice)
+    when 'A'
+      linespace
+      puts "Manufacturer name:"
+      brand = gets.chomp
+      new_brand = Brand.new({:brand => brand})
+      new_brand.save
+      puts "#{new_brand.brand} has been added!"
+      brand_menu
+    when 'L'
+      linespace
+      results = Brand.all
+      puts "All Manufacturers:"
+      results.each do |result|
+        puts "Brand: #{result.brand}"
+        linespace
+      end
+      brand_menu
+    when 'D'
+      linespace
+      puts "Manufacturer to remove:"
+      brand_to_remove = gets.chomp
+      results = Brand.all
+      results.each do |result|
+        if result.brand == brand_to_remove
+          puts "Brand #{result.brand} has been removed."
+          result.delete
+        else
+          puts "Result not found.  Did you type the name correctly?"
+        end
+      end
+      brand_menu
+    when 'R'
+      main_menu
+    else
+      puts "Invalid! Try again"
+  end
+end
 
 header
 main_menu
