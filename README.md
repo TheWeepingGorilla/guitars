16c00f15c5fd4be7626eb5bfa2371085328102eb
Sean Morrison <student@epicodus.com>
2014-08-10 14:09:28 -0700

    Guitar menu add delete and list functionality.

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 This is an example of an inventory program using a database to store data.
 
-It is simple but functional and could easily be expanded.
+It is simple but functional and demonstrates database concepts. 
+It was written in a style that allows expansion.
 
 Various characteristics that make up a guitar are kept in seperate tables and then aggregiated into
 a join table to represent each unique guitar model.
-
 Each characteristic and the guitars themselves can be listed, added, or deleted. 
+
+Possible additions: 
+
+Guitar types list themselves in full when added. (Break guitar display code into own function).
+Search and return all guitars by manufacturer, body style etc.
+Add additional guitar characteristics: model name, color, pickup config etc.
+
+
+Author: Sean Morrison
+License: Public Domain
+ 
+
--- lib/guitar_menu.rb
+++ lib/guitar_menu.rb
@@ -8,34 +8,47 @@ def guitar_menu
   choice = gets.chomp.upcase
   case(choice)
     when 'A'
+      new_body_id = 0
+      new_brand_id = 0
+      new_body_id = get_body
+      guitar_menu if new_body_id == 0
+      new_brand_id = get_brand
+      guitar_menu if new_brand_id == 0
+      new_guitar = Guitar.new({:brand_id => new_brand_id, :body_id => new_body_id})
+      new_guitar.save
       linespace
-      get_body_type
-
+      puts "New guitar model saved!"
       guitar_menu
     when 'L'
       linespace
-      results = Body.all
-      puts "All Body Types:"
+      results = Guitar.all
+      puts "All guitar models:"
       linespace
       results.each do |result|
-        puts "Body: #{result.body}"
-        linespace
+        brand_display = DB.exec("SELECT brand FROM brands WHERE id = #{result.brand_id};").first["brand"]
+        body_display = DB.exec("SELECT body FROM bodies WHERE id = #{result.body_id};").first["body"]
+        puts "#{result.id} #{brand_display} #{body_display}"
       end
-      body_menu
+      guitar_menu
     when 'D'
       linespace
-      puts "Body Type to remove:"
-      body_to_remove = gets.chomp
-      results = Body.all
+      puts "Id of guitar model to remove:"
+      id_to_remove = gets.chomp.to_i
+      results = Guitar.all
+      not_found = true
       results.each do |result|
-        if result.body == body_to_remove
-          puts "Body #{result.body} has been removed."
+        if result.id == id_to_remove
+          linespace
+          puts "Guitar #{result.id} has been removed."
           result.delete
-        else
-          puts "Result not found.  Did you type the name correctly?"
+          not_found = false
         end
       end
-      body_menu
+      if (not_found)
+        linespace
+        puts "** Model not found!  (L will list all guitar models)."
+      end
+      guitar_menu
     when 'R'
       main_menu
     else
@@ -43,20 +56,46 @@ def guitar_menu
   end
 end
 
-def get_body_type
+def get_body
   linespace
-  puts "Body Type of model:"
-  body_type = gets.chomp
   results = Body.all
-  p results
-
+  puts "All Body Types:"
   results.each do |result|
-    if result.body == body_type
-      puts "New Guitar model will have #{result.body}."
-      break
-    else
-      puts "Result not found.  Did you type the name correctly?"
-      break
+    puts "#{result.id} : #{result.body}"
+  end
+  linespace
+  puts "Enter id of body type:"
+  body_type_id = gets.chomp.to_i
+  results.each do |result|
+    if result.id == body_type_id
+      linespace
+      puts "New guitar model will have a #{result.body} body type."
+      return result.id
     end
   end
+  linespace
+  puts "** Not a valid body id."
+  return 0
+end
+
+def get_brand
+  linespace
+  results = Brand.all
+  puts "All Manufacturers:"
+  results.each do |result|
+    puts "#{result.id} : #{result.brand}"
+  end
+  linespace
+  puts "Enter id of manufacturer:"
+  brand_id = gets.chomp.to_i
+  results.each do |result|
+    if result.id == brand_id
+      linespace
+      puts "New guitar model is made by #{result.brand}."
+      return result.id
+    end
+  end
+  linespace
+  puts "** Not a valid manufacturer id."
+  return 0
 end
